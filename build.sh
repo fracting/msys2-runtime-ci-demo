5e336f5b0c00a906a32a8bfcdb1a10133b9b25d4
Qian Hong <qhong@codeweavers.com>
2015-06-19 06:38:17 +0800

    forward exit code from makepkg to main script.

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ pacman -Sy
 pacman -S --needed --noconfirm base-devel gcc
 makepkg -s --noconfirm --skippgpcheck -f
 
+echo $? > ~/build.log # store exit code of makepkg
+
 #clean up
 wineboot --kill # kill background wineconsole and socat processes
