4f764a00e855355eebf27e0164d574fc79ab93e3
Qian Hong <qhong@codeweavers.com>
2015-06-19 01:03:17 +0800

    Move build process to background, use wineboot --kill to kill socat process after build, use wineserver --wait to sync with build process and socat process.

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ ls
 pacman -Sy
 pacman -S --needed --noconfirm base-devel gcc
 makepkg -s --noconfirm --skippgpcheck -f
+
+#clean up
+wineboot --kill # kill background wineconsole and socat processes
