1e0296485c371a802fb7576c85e4e0aa769cb000
Qian Hong <qhong@codeweavers.com>
2015-06-19 19:23:32 +0800

    Also redirect stderr to log.

--- msys2_socat.sh
+++ msys2_socat.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-pacman -S --needed --noconfirm socat | tee -a ~/msys2_socat.log
+pacman -S --needed --noconfirm socat 2>&1 | tee -a ~/msys2_socat.log
 # echo for local debug
 echo socat tcp-listen:44444,fork,bind=127.0.0.1 exec:/usr/bin/bash,stderr | tee -a ~/msys2_socat.log
-socat tcp-listen:44444,fork,bind=127.0.0.1 exec:/usr/bin/bash,stderr | tee -a ~/msys2_socat.log
-echo socat error | tee -a ~/msys2_socat.log
+socat tcp-listen:44444,fork,bind=127.0.0.1 exec:/usr/bin/bash,stderr 2>&1 | tee -a ~/msys2_socat.log
+echo socat error - last command should not exit | tee -a ~/msys2_socat.log
