992d7e9d195f68d2be016e485d49a8843584d95e
Qian Hong <qhong@codeweavers.com>
2015-06-19 19:20:40 +0800

    Dummy commit

--- msys2_update.sh
+++ msys2_update.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+echo pacman -Sy | tee -a ~/msys2_update.log
 pacman -Sy | tee -a ~/msys2_update.log
+echo pacman -S --needed --noconfirm msys2-runtime pacman | tee -a ~/msys2_update.log
 pacman -S --needed --noconfirm msys2-runtime pacman | tee -a ~/msys2_update.log
