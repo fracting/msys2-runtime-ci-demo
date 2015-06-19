e7161371f362fbc7872bdb913fb9502d1a86fc16
Qian Hong <qhong@codeweavers.com>
2015-06-20 04:46:02 +0800

    Use --noprogressbar

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 echo pacman -Sy
 pacman -Sy
-echo pacman -S --needed --noconfirm base-devel gcc
-pacman -S --needed --noconfirm base-devel gcc
-echo makepkg -s --noconfirm --skippgpcheck -f
-makepkg -s --noconfirm --skippgpcheck -f
+echo pacman -S --needed --noconfirm --noprogressbar base-devel gcc
+pacman -S --needed --noconfirm --noprogressbar base-devel gcc
+echo makepkg -s --noconfirm --noprogressbar --skippgpcheck -f
+makepkg -s --noconfirm --noprogressbar --skippgpcheck -f
 
 status=$?
 echo $status | tee ~/build.log # store exit code of makepkg
