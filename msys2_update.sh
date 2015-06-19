baa06428371c3a14210f5017e3ed2f6ba4ccc43c
Qian Hong <qhong@codeweavers.com>
2015-06-19 19:21:55 +0800

    Also redirect stderr to log

--- msys2_update.sh
+++ msys2_update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo pacman -Sy | tee -a ~/msys2_update.log
-pacman -Sy | tee -a ~/msys2_update.log
-echo pacman -S --needed --noconfirm msys2-runtime pacman | tee -a ~/msys2_update.log
-pacman -S --needed --noconfirm msys2-runtime pacman | tee -a ~/msys2_update.log
+echo pacman -Sy
+pacman -Sy 2>&1 | tee -a ~/msys2_update.log
+echo pacman -S --needed --noconfirm msys2-runtime pacman
+pacman -S --needed --noconfirm msys2-runtime pacman 2>&1 | tee -a ~/msys2_update.log
