c24887aa8bd30f1d6f858601cc3f31653713bd85
Qian Hong <qhong@codeweavers.com>
2015-06-19 19:12:48 +0800

    Dummy commit

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+echo pacman -S --needed --noconfirm base-devel gcc
 pacman -S --needed --noconfirm base-devel gcc
+echo makepkg -s --noconfirm --skippgpcheck -f
 makepkg -s --noconfirm --skippgpcheck -f
 
 echo $? > ~/build.log # store exit code of makepkg
 
 #clean up
+echo wineboot --kill # kill background wineconsole and socat processes
 wineboot --kill # kill background wineconsole and socat processes
