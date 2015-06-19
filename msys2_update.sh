d0963ba3709516c7c3c1388aaa99a0784bf2b9a5
Qian Hong <qhong@codeweavers.com>
2015-06-19 19:24:36 +0800

    Also update pacman-mirrors package.

--- msys2_update.sh
+++ msys2_update.sh
@@ -2,4 +2,4 @@
 echo pacman -Sy
 pacman -Sy 2>&1 | tee -a ~/msys2_update.log
 echo pacman -S --needed --noconfirm msys2-runtime pacman
-pacman -S --needed --noconfirm msys2-runtime pacman 2>&1 | tee -a ~/msys2_update.log
+pacman -S --needed --noconfirm msys2-runtime pacman pacman-mirrors 2>&1 | tee -a ~/msys2_update.log
