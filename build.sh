3cd6d36a759a2b69d5e3e1a79bc35c813a3a47ce
Qian Hong <qhong@codeweavers.com>
2015-06-20 04:33:37 +0800

    Don't override exit code.

--- build.sh
+++ build.sh
@@ -7,8 +7,9 @@ pacman -S --needed --noconfirm base-devel gcc
 echo makepkg -s --noconfirm --skippgpcheck -f
 makepkg -s --noconfirm --skippgpcheck -f
 
-echo -n "exit code is "
-echo $? | tee ~/build.log # store exit code of makepkg
+status=$?
+echo $status | tee ~/build.log # store exit code of makepkg
+echo "exit code is $status"
 
 #clean up
 echo wineboot --kill # kill background wineconsole and socat processes
