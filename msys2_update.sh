28ec8f647b913b46171398894371461d275883c8
Qian Hong <qhong@codeweavers.com>
2015-06-21 00:39:07 +0800

    More debug message in msys2_update.sh.

--- msys2_update.sh
+++ msys2_update.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
+
+echo stat / 2>&1 | tee -a ~/msys2_update.log
+stat / 2>&1 | tee -a ~/msys2_update.log
+echo stat /dev 2>&1 | tee -a ~/msys2_update.log
+stat /dev 2>&1 | tee -a ~/msys2_update.log
+echo stat /dev/fd 2>&1 | tee -a ~/msys2_update.log
+stat /dev/fd 2>&1 | tee -a ~/msys2_update.log
+echo stat /proc 2>&1 | tee -a ~/msys2_update.log
+stat /proc 2>&1 | tee -a ~/msys2_update.log
+echo stat /proc/self 2>&1 | tee -a ~/msys2_update.log
+stat /proc/self 2>&1 | tee -a ~/msys2_update.log
+echo stat /proc/self/fd 2>&1 | tee -a ~/msys2_update.log
+stat /proc/self/fd 2>&1 | tee -a ~/msys2_update.log
+echo stat /proc/self/fd/* 2>&1 | tee -a ~/msys2_update.log
+stat /proc/self/fd/* 2>&1 | tee -a ~/msys2_update.log
+
+echo ls -lR /dev 2>&1 | tee -a ~/msys2_update.log
+ls -lR /dev 2>&1 | tee -a ~/msys2_update.log
+
+echo ls -lR /proc/self 2>&1 | tee -a ~/msys2_update.log
+ls -lR /proc/self 2>&1 | tee -a ~/msys2_update.log
+
+echo ls -lR /proc/self/ 2>&1 | tee -a ~/msys2_update.log
+ls -lR /proc/self/ 2>&1 | tee -a ~/msys2_update.log
+
+
+
 echo pacman -Sy
 pacman -Sy 2>&1 | tee -a ~/msys2_update.log
 echo pacman -S --needed --noconfirm msys2-runtime pacman
