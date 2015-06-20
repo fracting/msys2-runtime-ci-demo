bc0977e7c9e828ab575af004018163593404e887
Qian Hong <qhong@codeweavers.com>
2015-06-21 07:45:44 +0800

    Fix exit code handling.

--- build.sh
+++ build.sh
@@ -24,21 +24,19 @@ ls -lR /proc/self
 echo ls -lR /proc/self/
 ls -lR /proc/self/
 
-for i in {0..80}; do echo timestamp:$i; sleep 60; done 2>&1 | tee build.log & # background
+for i in {0..80}; do echo timestamp:$i; sleep 60; done 2>&1 | tee -a build.log & # background
 
-echo $ pacman -Sy 2>&1 | tee build.log
-pacman -Sy 2>&1 | tee build.log
-echo $ pacman -S --needed --noconfirm --noprogressbar base-devel gcc 2>&1 | tee build.log
-pacman -S --needed --noconfirm --noprogressbar base-devel gcc 2>&1 | tee build.log
-echo $ makepkg -s --noconfirm --noprogressbar --skippgpcheck -f 2>&1 | tee build.log
-makepkg -s --noconfirm --noprogressbar --skippgpcheck -f 2>&1 | tee build.log
+echo $ pacman -Sy 2>&1 | tee -a build.log
+pacman -Sy 2>&1 | tee -a build.log
+echo $ pacman -S --needed --noconfirm --noprogressbar base-devel gcc 2>&1 | tee -a build.log
+pacman -S --needed --noconfirm --noprogressbar base-devel gcc 2>&1 | tee -a build.log
+echo $ makepkg -s --noconfirm --noprogressbar --skippgpcheck -f 2>&1 | tee -a build.log
+(makepkg -s --noconfirm --noprogressbar --skippgpcheck -f; echo $? > ~/exit.number) 2>&1 | tee -a build.log
 
-status=$?
-echo $status | tee ~/exit.number # store exit code of makepkg
-echo "exit code is $status" 2>&1 | tee build.log
-echo "exit code is $status"
+exit_code=$(cat ~/exit.number)
+echo "exit code is $exit_code" 2>&1 | tee -a build.log
 
 #clean up
-echo wineboot --kill 2>&1 | tee build.log # kill background wineconsole and socat processes
+echo wineboot --kill 2>&1 | tee -a build.log # kill background wineconsole and socat processes
 wineboot --kill # kill background wineconsole and socat processes
-exit $status # restore exit code of makepkg
+exit $exit_code # restore exit code of makepkg
