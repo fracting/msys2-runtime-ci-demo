70511209fad6654f649b650ca2e4ccad936ac6f3
Qian Hong <qhong@codeweavers.com>
2015-06-23 22:14:05 +0800

    clean up.

--- msys2_update.sh
+++ msys2_update.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-
-echo pacman -Sy
-pacman -Sy 2>&1 | tee -a ~/msys2_update.log
-echo pacman -S --needed --noconfirm msys2-runtime pacman
-pacman -S --needed --noconfirm msys2-runtime pacman pacman-mirrors 2>&1 | tee -a ~/msys2_update.log
+echo $ pacman -Sy
+pacman -Sy
+echo $ pacman -S --needed --noconfirm msys2-runtime pacman pacman-mirrors
+pacman -S --needed --noconfirm msys2-runtime pacman pacman-mirrors
